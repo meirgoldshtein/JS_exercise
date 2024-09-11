5ae3ebce9749a30825c4db720d182c82081129f7
meirgoldshtein <m0548491057@gmail.com>
2024-09-11 16:26:52 +0300

    finish

--- test.js
+++ test.js
@@ -6,27 +6,27 @@ const filterAndSortEvenNumbers = (arr) => {
 const removeDuplicates = (arr) => {
     return arr.filter((item, index) => arr.indexOf(item) === index);
 }
-
+// הוספת אותיות גדולות למילים
 const capitalizeFirstLetter = (str) => {
     const arr = str.split(' ');
     return arr.map(word => word[word.length-1] != '.' ? word.charAt(0).toUpperCase() + word.slice(1) : word).join(' ');
 }
-
+// שמירת מערך משימות בלוקל סטורג
 const saveTasks = (arr) => {
     localStorage.setItem('tasks', JSON.stringify(arr));
 }
-
+// החזרת מערך המשימות מהלוקל סטורג
 const loadTasks = () => {
     return JSON.parse(localStorage.getItem('tasks'));
 }
-
+// הוספת אובייקט משימה למערך הלוקל סטורג
 const addTask = (task) => {
     const tasks = loadTasks() || [];
     tasks.push(task);
     saveTasks(tasks);
 }
-
-const removeTask = (task) => {
-    const tasks = loadTasks().filter(t => t.id != task.id);
+// מחיקת משימה מהמערך בלוקל סטורג
+const removeTask = (id) => {
+    const tasks = loadTasks().filter(t => t.id != id);
     saveTasks(tasks);
 }
